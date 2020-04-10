120824e35e97435a4354cd36bb8d109ce8ad6439
Aarre Leinonen <aarre.leinonen@tuni.fi>
2020-04-10 10:59:59 +0300

    tehty dokumenttia eteenpäin. lisätty operaatioita

--- harjoitustyo/dokumentit/Dokumentti.java
+++ harjoitustyo/dokumentit/Dokumentti.java
@@ -6,12 +6,36 @@ package harjoitustyo.dokumentit;
 */
 
 
-public abstract class Dokumentti {
+public abstract class Dokumentti implements Comparable<Dokumentti> {
 
     //Dokumenttien yhteiset tiedot
     private int tunniste;
     private String teksti;
 
 
+    //Aksessorit
+
+
+    //Rakentajat
+
+
+    @Override
+    public String toString() {
+        String palautettava;
+        palautettava = tunniste + "///" + teksti;
+        return palautettava;
+    }
+
+    @Override
+    public boolean equals(Object verrattava) {
+
+        return false;
+
+    }
+
+    public int compareTo(Dokumentti verrattava) {
+
+        return 1;
+    }
 
 }
